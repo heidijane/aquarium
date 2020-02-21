f05458a4e73bbf4725eda6a8edf79855fe1f369a
heidijane <heidi@heidisprouse.com>
2020-02-21 12:03:32 -0600

    Added Betty, Goober, and Seabiscuit to the fish list

--- scripts/dialogs.js
+++ scripts/dialogs.js
@@ -24,6 +24,32 @@ const initializeDetailButtonEvents = () => {
             theDialog.showModal()
         }
     )
+    // Show Betty's details when the button is clicked
+    document.querySelector("#button--betty").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--betty")
+            theDialog.showModal()
+        }
+    )
+
+     // Show Goober's details when the button is clicked
+     document.querySelector("#button--goober").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--goober")
+            theDialog.showModal()
+        }
+    )
+
+    // Show Goober's details when the button is clicked
+    document.querySelector("#button--seabiscuit").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--seabiscuit")
+            theDialog.showModal()
+        }
+    )
 
 }
 
