eef910b1ccde48eb62a0ae6a5aaee629efa4f838
heidijane <heidi@heidisprouse.com>
2020-02-26 13:57:06 -0600

    Added fish to FishDataProvider.js

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,67 @@
+const fishCollection = [
+    {
+        name: "Betty",
+        species: "Longtooth Barracuda",
+        length: "2 feet",
+        diet: ["squid", "humans"],
+        location: "Cumberland River"
+    },
+    {
+        name: "Goober",
+        species: "Clown Loach",
+        length: "7 inches",
+        diet: ["snails", "sinking pellets"],
+        location: "Sumatra"
+    },
+    {
+        name: "Seabiscuit",
+        species: "Leafy Sea Dragon",
+        length: "8 inches",
+        diet: ["plankton"],
+        location: "South Australia"
+    },
+    {
+        name: "Frank",
+        species: "Hammerhead Shark",
+        length: "15 feet",
+        diet: ["fish"],
+        location: "Florida Keys"
+    },
+    {
+        name: "Flarp",
+        species: "Blobfish",
+        length: "11 inches",
+        diet: ["???"],
+        location: "Tasmania"
+    },
+    {
+        name: "Jerry",
+        species: "Longhorn Cowfish",
+        length: "20 inches",
+        diet: ["crustaceans", "small fish", "jellyfish"],
+        location: "Ryukyu Islands"
+    },
+    {
+        name: "Nightmare",
+        species: "Anglerfish",
+        length: "5 inches",
+        diet: ["your soul"],
+        location: "Deep Sea"
+    },
+    {
+        name: "Kismet",
+        species: "Prairie Dog Fish",
+        length: "11 inches",
+        diet: ["timothy hay", "mealworms", "sweet potatoes", "socks"],
+        location: "Texas"
+    },
+    {
+        name: "Sparky",
+        species: "Neon Tetra",
+        length: "3/4 inch",
+        diet: ["fish flakes", "bloodworms", "sweet potatoes", "socks"],
+        location: "Peru"
+    }
+];
+
+console.log(fishCollection);
