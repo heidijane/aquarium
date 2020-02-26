c777aa529d1ec50afc5d0210f4fbda2ef76b5abc
heidijane <heidi@heidisprouse.com>
2020-02-26 14:10:37 -0600

    Added export function to FishDataProvider.js

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -4,64 +4,75 @@ const fishCollection = [
         species: "Longtooth Barracuda",
         length: "2 feet",
         diet: ["squid", "humans"],
-        location: "Cumberland River"
+        location: "Cumberland River",
+        image: "betty.jpg"
     },
     {
         name: "Goober",
         species: "Clown Loach",
         length: "7 inches",
         diet: ["snails", "sinking pellets"],
-        location: "Sumatra"
+        location: "Sumatra",
+        image: "clownloach.jpg"
     },
     {
         name: "Seabiscuit",
         species: "Leafy Sea Dragon",
         length: "8 inches",
         diet: ["plankton"],
-        location: "South Australia"
+        location: "South Australia",
+        image: "seadragon.jpg"
     },
     {
         name: "Frank",
         species: "Hammerhead Shark",
         length: "15 feet",
         diet: ["fish"],
-        location: "Florida Keys"
+        location: "Florida Keys",
+        image: "hammerhead.jpg"
     },
     {
         name: "Flarp",
         species: "Blobfish",
         length: "11 inches",
         diet: ["???"],
-        location: "Tasmania"
+        location: "Tasmania",
+        image: "blobfish.jpg"
     },
     {
         name: "Jerry",
         species: "Longhorn Cowfish",
         length: "20 inches",
         diet: ["crustaceans", "small fish", "jellyfish"],
-        location: "Ryukyu Islands"
+        location: "Ryukyu Islands",
+        image: "cowfish.jpg"
     },
     {
         name: "Nightmare",
         species: "Anglerfish",
         length: "5 inches",
         diet: ["your soul"],
-        location: "Deep Sea"
+        location: "Deep Sea",
+        image: "anglerfish.jpg"
     },
     {
         name: "Kismet",
         species: "Prairie Dog Fish",
         length: "11 inches",
         diet: ["timothy hay", "mealworms", "sweet potatoes", "socks"],
-        location: "Texas"
+        location: "Texas",
+        image: "kismet.jpg"
     },
     {
         name: "Sparky",
         species: "Neon Tetra",
         length: "3/4 inch",
         diet: ["fish flakes", "bloodworms", "sweet potatoes", "socks"],
-        location: "Peru"
+        location: "Peru",
+        image: "neontetra.jpg"
     }
 ];
 
-console.log(fishCollection);
+export const useFish = () => {
+    return fishCollection
+}
