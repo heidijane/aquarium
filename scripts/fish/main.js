13b43381ce073bc4c0dd2eb11c2c0a39ff306be4
heidijane <heidi@heidisprouse.com>
2020-02-27 14:13:30 -0600

    Moved main.js into scripts/fish for clarity

--- scripts/fish/main.js
+++ scripts/fish/main.js
@@ -1,6 +1,6 @@
-import initializeDetailButtonEvents from './dialogs.js';
-import FishList from './fish/FishList.js';
-import tipList from './tips/TipList.js';
+import initializeDetailButtonEvents from '../dialogs.js';
+import FishList from './FishList.js';
+import tipList from '../tips/TipList.js';
 // If the export is default no curly braces, if no default than put the function name in {}
 
 FishList(); //initialize the fish first before the buttons!
