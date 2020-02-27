775d2243aa4d6fab437b0add8493e35956c1ec09
heidijane <heidi@heidisprouse.com>
2020-02-27 12:00:20 -0600

    Added a comment in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@ import initializeDetailButtonEvents from './dialogs.js';
 import FishList from './FishList.js';
 // If the export is default no curly braces, if no default than put the function name in {}
 
-FishList();
+FishList(); //initialize the fish first before the buttons!
 
 initializeDetailButtonEvents();
