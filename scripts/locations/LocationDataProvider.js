39c51f4fd0cdb85cf923a7d8f536a10f6b70351d
heidijane <heidi@heidisprouse.com>
2020-02-27 14:51:10 -0600

    Added location images and added data for LocationDataProvider.js

---
+++ scripts/locations/LocationDataProvider.js
@@ -0,0 +1,50 @@
+cosnt locationCollection = [
+    {
+        name: "Cumberland River",
+        image: "cumberland_river.jpg",
+        country: "United States",
+        tips: ["Make sure to avoid barges!"]
+    },
+    {
+        name: "Sumatra",
+        image: "sumatra.jpg",
+        country: "Indonesia",
+        tips: ["You may encounter the putrid Corps Lily while hiking through the forest. Be warned!", "Also watch out for Tigers."]
+    },
+    {
+        name: "Great Australian Bight",
+        image: "australia.jpg",
+        country: "Australia",
+        tips: ["You may see a southern right whale whale on your dive!"];
+    },
+    {
+        name: "Florida Keys",
+        image: "floridakeys.jpg",
+        country: "United States",
+        tips: ["Watch out for the wild chickens that live all over the islands."]
+    },
+    {
+        name: "???",
+        image: "deepsea.jpg",
+        country: "???",
+        tips: ["You will need a deep sea submersible to harvest fish from the deep sea.", "The pressure in the deepest part of the ocean can be 1,071 more than standard atmospheric levels."]
+    },
+    {
+        name: "Ryukyu Islands",
+        image: "ryukyuislands.jpg",
+        country: "Japan",
+        tips: ["Typhoons are common, so be prepared!"]
+    },
+    {
+        name: "Texas",
+        image: "texas.jpg",
+        country: "United States",
+        tips: ["The lightning whelk is the official state shell of Texas!"]
+    },
+    {
+        name: "Amnesty Bay",
+        image: "amnestybay.jpg",
+        country: "United States",
+        tips: ["Don't forget to checkout the Curry Lighthouse, the famous local landmark!"]
+    }
+];
