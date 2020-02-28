cea16ff28931e5cf3f5c0b54700fb3ee33f205d9
heidijane <heidi@heidisprouse.com>
2020-02-28 12:00:41 -0600

    Fixed flip positon issue, styled back of cards

--- scripts/locations/Location.js
+++ scripts/locations/Location.js
@@ -1,22 +1,43 @@
 const Location = (location) => {
-    return `
+    let locHTML = `
         <section class="location location--card">
             <div id="card--${location.locid}" class="card">
+
+            <!--front of card-->
                 <div class="card__face card__face--front">
-                    <div>
-                        <img class="location__image"
-                    src="images/locations/${location.image}"
-                    alt="${location.name}">
+                    <div class="card__spacing">
+                        <div>
+                            <img class="location__image"
+                        src="images/locations/${location.image}"
+                        alt="${location.name}">
+                        </div>
+                        <div class="location__name">${location.name}</div>
                     </div>
-                    <div class="location__name">${location.name}</div>
                 </div>
+
+                <!--back of card-->
                 <div class="card__face card__face--back">
-                    <div class="location__country">${location.country}</div>
-                    <div class="location__tips">${location.tips.join(",")}</div>
+                    <div class="card__spacing">
+                        <div class="location__country">${location.country}</div>
+                        <div class="location__tips">
+                            <ul>`;
+                        for (let locationTip of location.tips) {
+                            locHTML += `<li>${locationTip}</li>`;
+                        }
+                        
+        locHTML += `                
+                        
+                            </ul>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </section
-                `
+                `;
+
+        return locHTML;
+        
 }
 
 export default Location
